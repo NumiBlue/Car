54ee987c38fca3b7cffa93f5e19ef4b2e2307291
NumiBlue <thekindlyblog@gmail.com>
2023-05-09 10:32:26 -0400

    first commit of day

--- controls.js
+++ controls.js
@@ -24,6 +24,7 @@ class Controls{
                     this.reverse=true;
                     break;
             }
+           // console.table(this);
         }
         document.onkeyup=(event)=>{
             switch(event.key){
@@ -40,6 +41,7 @@ class Controls{
                     this.reverse=false;
                     break;
             }
+           // console.table(this);
         }
     }
 }

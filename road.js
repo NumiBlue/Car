081b6870690e58d29eb690da5d0fdeda4af416c4
NumiBlue <thekindlyblog@gmail.com>
2023-05-25 03:58:19 -0400

    adds utils.js

--- road.js
+++ road.js
@@ -16,14 +16,17 @@ class Road {
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
+        for(let i=0;i<=this.laneCount;i++){
+           const x=lerp(
+            this.left,
+            this.right,
+            i/this.laneCount
+           );
+        
         ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
+        ctx.moveTo(x,this.top);
+        ctx.lineTo(x,this.bottom);
         ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
+           }
     }
 }
